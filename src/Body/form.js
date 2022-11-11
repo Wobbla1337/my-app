ccf8801c7a94e2f08e7b869dae10d01e3a00e482
JT <rymkah@gmail.com>
2022-11-11 16:58:09 +0200

    dates

--- src/Body/form.js
+++ src/Body/form.js
@@ -1,31 +1,41 @@
+import { useState } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function FromComponent({ show, handleClose }) {
+
+    const [startDateFrom, setStartDateFrom] = useState(new Date());
+    const [startDateTo, setStartDateTo] = useState(new Date());
+    const dateFormat = 'dd-MM-yyyy';
+
     const languages = [
         { label: 'English', code: 'en' },
         { label: 'Russian', code: 'ru' },
         { label: 'Germany', code: 'de' },
         { label: 'Franch', code: 'fr' },
-        { label: 'Spanish', code: 'es'},
+        { label: 'Spanish', code: 'es' },
     ];
 
     function capitalize(str) {
         return str[0].toUpperCase() + str.substring(1);
     };
-  
+
     function handleSubmit(event) {
-      event.preventDefault();
-    
-    const data = {
-        q: event.target.q.value,
-        from: event.target.from.value,
-        to: event.target.to.value,
-        language: event.target.language.value,
-    };
-    console.log(data);
+        event.preventDefault();
+
+        const data = {
+            q: event.target.q.value,
+            from: moment(event.target.from.value, dateFormat).format("YYYY-MM-DDT00:00:00.000"),
+            to: moment(event.target.from.value, dateFormat).format("YYYY-MM-DDT23:59:59.999"),
+            language: event.target.language.value,
+            searchIn: [...event.target.searchIn].filter(input => input.checked).map(input => input.value).join(','),
+        };
+        console.log(data);
 
     }
 
@@ -51,6 +61,7 @@ function FromComponent({ show, handleClose }) {
                                 // css: text-transform: capitalize;
                                 name="searchIn"
                                 type="checkbox"
+                                value={type}
                                 id={`inline-${type}-1`}
                             />
                         </div>
@@ -58,15 +69,27 @@ function FromComponent({ show, handleClose }) {
                     <Form.Group className="mb-3">
                         <Form.Label>From-to</Form.Label>
                         <InputGroup className="mb-3">
-                            <Form.Control aria-label="from" name="from" />
-                            <Form.Control aria-label="to" name="to" />
+                            <DatePicker
+                                className="form-control"
+                                selected={startDateFrom}
+                                onChange={(date) => setStartDateFrom(date)}
+                                name="from"
+                                dateFormat={dateFormat}
+                            />
+                            <DatePicker
+                                className="form-control"
+                                selected={startDateTo}
+                                onChange={(date) => setStartDateTo(date)}
+                                name="to"
+                                dateFormat={dateFormat}
+                            />
                         </InputGroup>
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label>Select Language</Form.Label>
                         <Form.Select name="language">
                             {languages.map((lang) => (
-                                <option>{lang.label}</option>
+                                <option key={lang.code} value={lang.code}>{lang.label}</option>
                             ))}
                         </Form.Select>
                     </Form.Group>
@@ -76,7 +99,7 @@ function FromComponent({ show, handleClose }) {
                 </Form>
             </Offcanvas.Body>
         </Offcanvas>
-         
-                            );
+
+    );
 }
 export default FromComponent;
