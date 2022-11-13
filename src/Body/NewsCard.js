04d836c42ca03ac3f4065a3d1d20fb17f514726e
JT <rymkah@gmail.com>
2022-11-13 21:33:29 +0200

    hw3

--- src/Body/NewsCard.js
+++ src/Body/NewsCard.js
@@ -31,5 +31,4 @@ function NewsCardComponent() {
     </>
   );
 }
-
 export default NewsCardComponent;
--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,17 +1,19 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import Pagination from 'react-bootstrap/Pagination';
+
+
+
 
 function PaginationComponent() {
 
-  const [page,setPage] = useState(1);
-
-
+  const [page, setPage] = useState(1);
+  
   return (
     <Pagination className="mt-4 justify-content-center">
-      <Pagination.Prev onClick={() => setPage(page-1)}/>
-       <Pagination.Item disabled>{page} / 140</Pagination.Item>  
-      <Pagination.Next onClick={() => setPage(page+1)}/>
-          </Pagination>
+      <Pagination.Prev disabled={page <=1} onClick={() => setPage(page - 1)} />
+      <Pagination.Item disabled>{page} / 140</Pagination.Item>
+      <Pagination.Next disabled={page>=140} onClick={() => setPage(page + 1)} />
+    </Pagination>
   );
 }
 
